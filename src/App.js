4d40e83d9477c67c227670fbcfe4f1f99f9ea5ee
Israel <israeladefidipe@gmail.com>
2021-09-18 12:48:49 +0100

    checkbox and todo text input styling done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import Header from './component/Header';
+import Todo from './component/Todo';
 import './styles/App.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App dark-mode">
       <div className="main-container">
         <Header />
+        <Todo />
       </div>
     </div>
   );
